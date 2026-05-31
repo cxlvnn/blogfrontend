fbdd1fbe047bbd1da3bb4da483f1dd8e0b68b0b6
cxlvnn <behruzturabekov19@gmail.com>
2026-06-01 00:25:13 +0500

    installed new package for icons, replaced the heart icon and added bookmark in post view, yet to be implemented fully

--- src/components/PostComponents/PostView.vue
+++ src/components/PostComponents/PostView.vue
@@ -31,20 +31,37 @@
       <p v-html="content"></p>
     </div>
 
-    <div
-      @click="likePost()"
-      :class="
-        liked
-          ? 'border-blue-500 text-blue-400'
-          : 'hover:border-neutral-600 border-neutral-800 text-neutral-400'
-      "
-      class="flex text-xs transition-colors hover:cursor-pointer items-center rounded border my-4 w-fit gap-2 py-2 px-4 bg-neutral-900"
-    >
-      <div>
-        <span>♥</span>
+    <div class="flex flex-row items-center justify-between gap-2">
+      <div
+        @click="likePost()"
+        :class="
+          liked
+            ? 'border-blue-500 text-blue-400 hover:text-blue-500'
+            : 'hover:border-neutral-600 border-neutral-800 text-neutral-400'
+        "
+        class="flex transition-colors hover:cursor-pointer items-center rounded border my-4 w-fit gap-2 py-2 px-4 bg-neutral-900"
+      >
+        <div class="text-sm">
+          <i
+            class="pi"
+            :class="liked ? 'pi pi-thumbs-up-fill' : 'pi pi-thumbs-up'"
+          ></i>
+        </div>
+        <div class="">
+          <p>{{ like_count }}</p>
+        </div>
       </div>
-      <div class="text-sm">
-        <p>{{ like_count }}</p>
+
+      <div
+        @click="savePost()"
+        :class="
+          saved
+            ? 'border-blue-500 text-blue-500'
+            : 'border-neutral-800 hover:border-neutral-700'
+        "
+        class="bg-neutral-900 border transition-colors rounded hover:cursor-pointer py-2 px-4"
+      >
+        <i class="pi" :class="saved ? 'pi-bookmark-fill' : 'pi-bookmark'"></i>
       </div>
     </div>
   </div>
@@ -66,25 +83,17 @@ const post = defineProps({
   userLiked: String,
 });
 
+const saved = ref(false);
+const savePost = async () => {
+  saved.value = !saved.value;
+};
+
 const liked = ref(false);
 const like_count = ref(post.likeCount);
 const user_liked = ref(post.userLiked);
 if (user_liked.value === "true") {
   liked.value = true;
 }
-
-const getNameLetters = (author) => {
-  if (!author || typeof author !== "string") {
-    return "";
-  }
-  let letters = author[0];
-  const splits = author.split(" ");
-  if (splits.length > 1) {
-    letters += splits[1][0];
-  }
-  return letters;
-};
-
 const likePost = async () => {
   liked.value = !liked.value;
   if (liked.value === true) {
@@ -97,5 +106,17 @@ const likePost = async () => {
   } catch (error) {
     console.error("Error liking the post", error);
   }
+};
+
+const getNameLetters = (author) => {
+  if (!author || typeof author !== "string") {
+    return "";
+  }
+  let letters = author[0];
+  const splits = author.split(" ");
+  if (splits.length > 1) {
+    letters += splits[1][0];
+  }
+  return letters;
 };
 </script>
