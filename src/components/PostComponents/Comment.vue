4fa69042804f1055e697bc62332a991a79d1e83e
cxlvnn <behruzturabekov19@gmail.com>
2026-06-05 15:17:45 +0500

    comment support: displaying and creating comments

---
+++ src/components/PostComponents/Comment.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="flex flex-row items-center gap-2 pb-4">
+    <div
+      class="w-10 h-10 border border-neutral-800 rounded-full bg-gray-400/10 flex items-center justify-center font-medium text-white/60"
+    >
+      {{ getNameLetters(authorName) }}
+    </div>
+
+    <div>
+      <div class="flex items-center gap-2">
+        <h1 class="font-medium text-md">
+          {{ authorName }}
+        </h1>
+
+        <p class="text-xs font-medium text-neutral-600">{{ date }}</p>
+      </div>
+
+      <div>
+        <p class="text-sm text-neutral-300">
+          {{ body }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  id: Number,
+  authorName: String,
+  date: String,
+  body: String,
+});
+
+const getNameLetters = (name) => {
+  if (!name || typeof name !== "string") {
+    return "";
+  }
+  let letters = name[0];
+  const splits = name.split(" ");
+  if (splits.length > 1) {
+    letters += splits[1][0];
+  }
+  return letters;
+};
+</script>
--- src/views/Post.vue
+++ src/views/Post.vue
@@ -17,14 +17,45 @@
       v-model="author"
     />
   </div>
+  <div class="flex flex-col gap-4 my-5 text-white">
+    <h1 class="font-medium uppercase text-neutral-500">Comments</h1>
+
+    <div class="border-b border-white/15" v-show="comments?.length" v-for="comment in comments">
+      <Comment
+        :authorName="comment.relationships?.authorName"
+        :body="comment.body"
+        :date="comment.postedAt"
+        :id="comment.id"
+      />
+    </div>
+    <div v-if="comments.length === 0">
+      <p class="text-neutral-600">No comments yet</p>
+    </div>
+
+    <div class="mt-5">
+      <h1 class="font-medium text-neutral-300 text-sm">Add comment</h1>
+
+      <form @submit.prevent="createComment(comment)">
+        <textarea
+          class="w-full border border-white/15 bg-neutral-900/20 rounded my-2 h-20 p-2 outline-none focus:border-blue-500"
+          v-model="comment.body"
+        />
+        <div class="flex justify-end">
+          <Button design="secondary" title="Post comment" />
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import api from "@/api/axios";
 import BackButton from "@/components/GlobalComponents/BackButton.vue";
+import Button from "@/components/GlobalComponents/Button.vue";
 import PostLoading from "@/components/GlobalComponents/LoadingComponents/PostLoading.vue";
+import Comment from "@/components/PostComponents/Comment.vue";
 import Post from "@/components/PostComponents/PostView.vue";
-import { onMounted, ref } from "vue";
+import { onMounted, reactive, ref } from "vue";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
@@ -46,4 +77,32 @@ onMounted(async () => {
     isLoading.value = false;
   }
 });
+
+const comments = ref([]);
+
+const getComments = async () => {
+  try {
+    const response = await api.get(`/posts/${id}/comments`);
+    comments.value = response.data.data;
+  } catch (error) {
+    console.error("Error feching comments", error);
+  }
+};
+
+onMounted(() => {
+  getComments();
+});
+
+const comment = reactive({
+  body: "",
+});
+
+const createComment = async (comment) => {
+  try {
+    const response = await api.post(`/posts/${id}/comments`, comment);
+    comments.value.push(response.data.data);
+  } catch (error) {
+    console.error("Error creating the comment", error);
+  }
+};
 </script>
