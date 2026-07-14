77a57a0a2eb81f4ebe8aaae2372c89b53865bdef
cxlvnn <behruzturabekov19@gmail.com>
2026-07-14 23:02:06 +0500

    search tab initial setup

---
+++ src/components/GlobalComponents/SearchBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="my-4">
+    <Input placeholder="Search posts..." />
+
+    <div class="text-neutral-400 font-medium flex gap-2">
+      <div
+        class="mt-4 mb-2 border border-neutral-700 px-3 py-1 rounded-2xl text text-xs hover:text-white hover:bg-neutral-700 hover:cursor-pointer"
+        v-for="tag in tags"
+      >
+        <p>{{ tag.name }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Input from "@/components/GlobalComponents/Input.vue";
+
+const tags = [
+  {
+    name: "All",
+  },
+  {
+    name: "Design",
+  },
+  {
+    name: "Productivity",
+  },
+  {
+    name: "Engineering",
+  },
+  {
+    name: "Writing",
+  },
+];
+</script>
--- src/components/PostComponents/PostsIndex.vue
+++ src/components/PostComponents/PostsIndex.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Posts from "@/components/PostComponents/Posts.vue";
+import SearchBar from "../GlobalComponents/SearchBar.vue";
 </script>
 
 <template>
   <main>
+    <SearchBar />
     <Posts />
   </main>
 </template>
